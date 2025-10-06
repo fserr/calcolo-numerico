40c614e746279eb8f37379e63e01f64011caa958
Federico Serra <fserra8@gmail.com>
2025-10-06 12:59:43 +0200

    repo init

---
+++ test.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# Definiamo un vettore v e una matrice A
+v = np.array([1, 1, 3, 2])
+A = np.array([[1, 1, -1],
+              [0, -1, 1]])
+
+# Stampiamo la shape
+print(f"La shape di v è: {v.shape}.")
+print(f"La shape di A è: {A.shape}.")
